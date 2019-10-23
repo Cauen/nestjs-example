a89e2f4c04aad51b0aba34ca223318c82bd5504f
adsonrocha <&a27%d33>
2019-10-22 21:59:16 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://adsonrocha:123456@cluster0-5efft.mongodb.net/test?retryWrites=true&w=majority'),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
